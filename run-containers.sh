ee19582da36ef004727e6e28bbdd667158b43702
webpwnized <webpwnized@gmail.com>
2022-08-17 19:11:38 -0400

    1.0.7 Added script stop-containers.sh

--- run-containers.sh
+++ run-containers.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "";
 echo "Starting containers";
 docker-compose -f docker-compose.yml up -d;
 
+echo "";
 echo "Waiting for database to start";
 sleep 10;
 
+echo "";
 echo "Requesting database be built";
 curl http://mutillidae.local/set-up-database.php;
 
+echo "";
 echo "Clearing the screen";
 clear;
