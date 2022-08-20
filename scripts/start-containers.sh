21d0f202a0fc6a282f21175042d8a7d8fa8b9d7c
webpwnized <webpwnized@gmail.com>
2022-08-20 15:21:19 -0400

    1.0.10 Automatically load LDIF to directory

--- scripts/start-containers.sh
+++ scripts/start-containers.sh
@@ -9,8 +9,15 @@ echo "Waiting for database to start";
 sleep 10;
 
 echo "";
-echo "Requesting database be built";
+echo "Requesting Mutillidae database be built";
 curl http://mutillidae.local/set-up-database.php;
+
+echo "";
+echo "Uploading Mutillidae LDIF file to LDAP directory server";
+ldapadd -c -x -D "cn=admin,dc=mutillidae,dc=local" -w mutillidae -H ldap:// -f ../ldif/mutillidae.ldif;
+
+# Wait for the user to press Enter key
+read -p "Press Enter to continue or <CTRL>-C to stop" </dev/tty
 
 echo "";
 echo "Clearing the screen";
